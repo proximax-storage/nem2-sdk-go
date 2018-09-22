e30fc1e0ff44582273423629096d86298773cf6c
slackve <eleazartovar123@gmail.com>
2018-09-21 22:05:40 -0400

    Add websocket examples

--- examples/websocket/main.go
+++ examples/websocket/main.go
@@ -23,25 +23,40 @@ func main() {
 	}
 	fmt.Println("websocket negotiated uid:", client.Uid)
 
-	//a, err := client.Subscribe.UnConfirmedAdded("SCFWMP2M2HP43KJYGOBDVQ3SKX3Q6HFH6HZZ6DNR")
-	//for {
-	//	data := <-a
-	//	fmt.Println(string(data))
-	//	var datainfo sdk.StatusInfo
-	//	err := json.Unmarshal(data, &datainfo)
-	//	if err != nil {
-	//		fmt.Println(err)
-	//		return
-	//	}
-	//	r, err := json.MarshalIndent(datainfo, "", "  ")
-	//	fmt.Println(string(r))
-	//}
-	//
-	//// Info If the transaction fails
-	//b, err := client.Subscribe.Status("SDXJKWQ5RFTBRQKPJTAB3OIFJFIMLLWMMAUNPXLZ")
+	//b, err := client.Subscribe.ConfirmedAdded("SCFWMP2M2HP43KJYGOBDVQ3SKX3Q6HFH6HZZ6DNR")
 	//for {
 	//	data := <-b
-	//
+	//	fmt.Println(string(data))
+	//}
+
+	//c, err := client.Subscribe.UnConfirmedAdded("SCFWMP2M2HP43KJYGOBDVQ3SKX3Q6HFH6HZZ6DNR")
+	//for {
+	//	data := <-c
+	//	fmt.Println(string(data))
+	//}
+
+	//d, err := client.Subscribe.UnconfirmedRemoved("SCFWMP2M2HP43KJYGOBDVQ3SKX3Q6HFH6HZZ6DNR")
+	//for {
+	//	data := <-d
+	//	fmt.Println(string(data))
+	//}
+
+	//e, err := client.Subscribe.PartialAdded("SCFWMP2M2HP43KJYGOBDVQ3SKX3Q6HFH6HZZ6DNR")
+	//for {
+	//	data := <-e
+	//	fmt.Println(string(data))
+	//}
+
+	//f, err := client.Subscribe.PartialRemoved("SCFWMP2M2HP43KJYGOBDVQ3SKX3Q6HFH6HZZ6DNR")
+	//for {
+	//	data := <-f
+	//	fmt.Println(string(data))
+	//}
+
+	//// Info If the transaction fails
+	//a, err := client.Subscribe.Status("SDXJKWQ5RFTBRQKPJTAB3OIFJFIMLLWMMAUNPXLZ")
+	//for {
+	//	data := <-a
 	//	var datainfo sdk.StatusInfo
 	//	err := json.Unmarshal(data, &datainfo)
 	//	if err != nil {
