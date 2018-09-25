fbc3883d95765f4a2bcee6ce8169e71983ff5da3
slackve <eleazartovar123@gmail.com>
2018-09-25 00:46:53 -0400

    add example NewConnectWs

--- examples/websocket/main.go
+++ examples/websocket/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/proximax-storage/nem2-sdk-go/sdk"
 )
@@ -14,71 +13,25 @@ func main() {
 		panic(err)
 	}
 
-	// Use the default websocket client
-	client := sdk.NewClientWs(nil, conf)
-
-	err = client.WsConnect()
+	client, err := sdk.NewConnectWs(conf)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("websocket negotiated uid:", client.Uid)
 
-	//b, err := client.Subscribe.ConfirmedAdded("SCFWMP2M2HP43KJYGOBDVQ3SKX3Q6HFH6HZZ6DNR")
-	//for {
-	//	data := <-b
-	//	fmt.Println(string(data))
-	//}
+	a, _ := client.Subscribe.Status("SCFWMP2M2HP43KJYGOBDVQ3SKX3Q6HFH6HZZ6DNR")
 
-	//c, err := client.Subscribe.UnConfirmedAdded("SCFWMP2M2HP43KJYGOBDVQ3SKX3Q6HFH6HZZ6DNR")
-	//for {
-	//	data := <-c
-	//	fmt.Println(string(data))
-	//}
-
-	//d, err := client.Subscribe.UnconfirmedRemoved("SCFWMP2M2HP43KJYGOBDVQ3SKX3Q6HFH6HZZ6DNR")
-	//for {
-	//	data := <-d
-	//	fmt.Println(string(data))
-	//}
-
-	//e, err := client.Subscribe.PartialAdded("SCFWMP2M2HP43KJYGOBDVQ3SKX3Q6HFH6HZZ6DNR")
-	//for {
-	//	data := <-e
-	//	fmt.Println(string(data))
-	//}
-
-	//f, err := client.Subscribe.PartialRemoved("SCFWMP2M2HP43KJYGOBDVQ3SKX3Q6HFH6HZZ6DNR")
-	//for {
-	//	data := <-f
-	//	fmt.Println(string(data))
-	//}
-
-	//// Info If the transaction fails
-	//a, err := client.Subscribe.Status("SDXJKWQ5RFTBRQKPJTAB3OIFJFIMLLWMMAUNPXLZ")
-	//for {
-	//	data := <-a
-	//	var datainfo sdk.StatusInfo
-	//	err := json.Unmarshal(data, &datainfo)
-	//	if err != nil {
-	//		fmt.Println(err)
-	//		return
-	//	}
-	//	r, err := json.MarshalIndent(datainfo, "", "  ")
-	//	fmt.Println(string(r))
-	//}
+	go func() {
+		for {
+			fmt.Printf("Status: %s \n", <-a.ChIn)
+			//a.Unsubscribe()
+		}
+	}()
 
 	// info once a block is created.
-	c, err := client.Subscribe.Block()
-	for {
-		data := <-c
+	c, _ := client.Subscribe.Block()
 
-		var datainfo sdk.BlockInfo
-		err := json.Unmarshal(data, &datainfo)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		r, err := json.MarshalIndent(datainfo, "", "  ")
-		fmt.Println(string(r))
+	for {
+		fmt.Printf("Block: %s \n", <-c.ChIn)
 	}
 }
