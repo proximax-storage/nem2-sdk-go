64fd3b22fdd49046645c66d86f5535b6e849f3ce
slackve <eleazartovar123@gmail.com>
2018-10-09 15:37:40 -0400

    Documentation of the AccountService methods

--- sdk/account.go
+++ sdk/account.go
@@ -20,6 +20,7 @@ const (
 	multisigAccountGraphInfoRoute = "multisig/graph"
 )
 
+// Gets an AccountInfo for an account.
 func (a *AccountService) GetAccountInfo(ctx context.Context, address *Address) (*AccountInfo, *http.Response, error) {
 	dto := &accountInfoDTO{}
 
@@ -36,6 +37,7 @@ func (a *AccountService) GetAccountInfo(ctx context.Context, address *Address) (
 	return acc, resp, nil
 }
 
+// Gets AccountsInfo for different accounts.
 func (a *AccountService) GetAccountsInfo(ctx context.Context, addresses []*Address) ([]*AccountInfo, *http.Response, error) {
 	ads := make([]string, len(addresses))
 	for i, ad := range addresses {
@@ -62,6 +64,7 @@ func (a *AccountService) GetAccountsInfo(ctx context.Context, addresses []*Addre
 	return infos, resp, nil
 }
 
+// Gets a MultisigAccountInfo for an account.
 func (a *AccountService) GetMultisigAccountInfo(ctx context.Context, address *Address) (*MultisigAccountInfo, *http.Response, error) {
 	dto := &multisigAccountInfoDTO{}
 	resp, err := a.client.DoNewRequest(ctx, "GET", fmt.Sprintf("%s/%s/%s", mainAccountRoute, address.Address, multisigAccountInfoRoute), nil, dto)
@@ -77,6 +80,7 @@ func (a *AccountService) GetMultisigAccountInfo(ctx context.Context, address *Ad
 	return info, resp, nil
 }
 
+// Gets a MultisigAccountGraphInfo for an account.
 func (a *AccountService) GetMultisigAccountGraphInfo(ctx context.Context, address *Address) (*MultisigAccountGraphInfo, *http.Response, error) {
 	dto := &multisigAccountGraphInfoDTOS{}
 	resp, err := a.client.DoNewRequest(ctx, "GET", fmt.Sprintf("%s/%s/%s", mainAccountRoute, address.Address, multisigAccountGraphInfoRoute), nil, dto)
@@ -92,22 +96,32 @@ func (a *AccountService) GetMultisigAccountGraphInfo(ctx context.Context, addres
 	return info, resp, nil
 }
 
+// Gets an array of confirmed transactions for which an account is signer or receiver.
 func (a *AccountService) Transactions(ctx context.Context, account *PublicAccount, opt *AccountTransactionsOption) ([]Transaction, *http.Response, error) {
 	return a.findTransactions(ctx, account, opt, transactionsRoute)
 }
 
+// Gets an array of transactions for which an account is the recipient of a transaction.
+// A transaction is said to be incoming with respect to an account if the account is the recipient of a transaction.
 func (a *AccountService) IncomingTransactions(ctx context.Context, account *PublicAccount, opt *AccountTransactionsOption) ([]Transaction, *http.Response, error) {
 	return a.findTransactions(ctx, account, opt, incomingTransactionsRoute)
 }
 
+// Gets an array of transactions for which an account is the sender a transaction.
+// A transaction is said to be outgoing with respect to an account if the account is the sender of a transaction.
 func (a *AccountService) OutgoingTransactions(ctx context.Context, account *PublicAccount, opt *AccountTransactionsOption) ([]Transaction, *http.Response, error) {
 	return a.findTransactions(ctx, account, opt, outgoingTransactionsRoute)
 }
 
+// Gets the array of transactions for which an account is the sender or receiver and which have not yet been included in a block.
+// Unconfirmed transactions are those transactions that have not yet been included in a block.
+// Unconfirmed transactions are not guaranteed to be included in any block.
 func (a *AccountService) UnconfirmedTransactions(ctx context.Context, account *PublicAccount, opt *AccountTransactionsOption) ([]Transaction, *http.Response, error) {
 	return a.findTransactions(ctx, account, opt, unconfirmedTransactionsRoute)
 }
 
+// Gets an array of transactions for which an account is the sender or has sign the transaction.
+// A transaction is said to be aggregate bonded with respect to an account if there are missing signatures.
 func (a *AccountService) AggregateBondedTransactions(ctx context.Context, account *PublicAccount, opt *AccountTransactionsOption) ([]*AggregateTransaction, *http.Response, error) {
 	txs, resp, err := a.findTransactions(ctx, account, opt, aggregateTransactionsRoute)
 
