38148ce7e05e9c17f1edbc765b72983f16f3c93d
BramBear <alvin.reyes@botmill.io>
2018-10-10 21:34:25 -0400

    Update sdk_schema_test.go

--- sdk/sdk_schema_test.go
+++ sdk/sdk_schema_test.go
@@ -1,4 +1,5 @@
-// Copyright 2018 ProximaX Limited. All rights reserved. // Use of this source code is governed by the Apache 2.0 // license that can be found in the LICENSE file.  package sdk
+// Copyright 2018 ProximaX Limited. All rights reserved. // Use of this source code is governed by the Apache 2.0 // license that can be found in the LICENSE file.  
+package sdk
 
 import (
 	"reflect"
