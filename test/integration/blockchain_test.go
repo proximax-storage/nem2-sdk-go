a7fb0c3bc1b65f915f3327862ab4a76e80dd29ed
Carlo Cayos <carlocayos@gmail.com>
2019-01-10 15:07:54 +1100

    integration test initial codes

---
+++ test/integration/blockchain_test.go
@@ -0,0 +1,22 @@
+// Copyright 2018 ProximaX Limited. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// +build integration
+package integration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBlockchain(t *testing.T) {
+
+	fmt.Print("Hello World")
+
+}
+
+
+
+
+
